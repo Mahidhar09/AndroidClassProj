2e5d932a7069c3b30937185985b6a08d2a90a230
pbonar <piotr.bonar@gmail.com>
2023-12-13 17:17:43 +0100

    adding images page and call page

--- app/src/main/java/com/example/start/CallPage.kt
+++ app/src/main/java/com/example/start/CallPage.kt
@@ -1,12 +1,55 @@
-package com.example.start
-
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Text
+import android.content.Intent
+import android.net.Uri
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.aspectRatio
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Call
+import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
 
 @Composable
 fun CallPage() {
-    // Content for the Call page
-    // Replace with your actual content
-    Text("Call Page Content", style = MaterialTheme.typography.headlineSmall)
+    val phoneNumber = "+33753060044"
+
+    val context = LocalContext.current
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(16.dp),
+        verticalArrangement = Arrangement.Center
+    ) {
+        Text("Call your beloved relative", style = MaterialTheme.typography.headlineSmall)
+
+        Spacer(modifier = Modifier.height(30.dp))
+
+        Button(
+            onClick = {
+                val dialIntent = Intent(Intent.ACTION_DIAL, Uri.parse("tel:$phoneNumber"))
+                context.startActivity(dialIntent)
+            },
+            content = {
+                Row {
+                    Text("+33753060044")
+                    Spacer(modifier = Modifier.width(9.dp))
+                    Icon(imageVector = Icons.Default.Call, contentDescription = null)
+                }
+            }
+        )
+    }
 }
--- app/src/main/java/com/example/start/ImagesPage.kt
+++ app/src/main/java/com/example/start/ImagesPage.kt
@@ -1,12 +1,61 @@
 package com.example.start
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 
 @Composable
 fun ImagesPage() {
-    // Placeholder content for the Images page
-    // Replace with your actual content
+    val hapihapihapi = listOf(
+        R.drawable.happy_family,
+        R.drawable.happy_family_1,
+        R.drawable.happy_family_2,
+        R.drawable.happy_family_3,
+        R.drawable.happy_family_4,
+        R.drawable.happy_family_5,
+        R.drawable.happy_family_6
+        )
+
     Text("Images Page Content", style = MaterialTheme.typography.headlineSmall)
+
+    LazyColumn {
+        items(hapihapihapi.chunked(2)) { rowImages ->
+            Row(
+                modifier = Modifier
+                    .padding(10.dp)
+                    .fillMaxWidth(),
+                horizontalArrangement = Arrangement.spacedBy(8.dp)
+            ) {
+                for (hapi in rowImages) {
+                    Image(
+                        contentDescription = null,
+                        painter = painterResource(id = hapi),
+                        modifier = Modifier
+                            .weight(1f)
+                            .aspectRatio(1f)
+                            .clip(MaterialTheme.shapes.medium),
+                        contentScale = ContentScale.Crop
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Preview
+@Composable
+fun ImagesPagePreview() {
+    // You can use this preview function to see how your UI looks
+    // Replace with your actual preview content
+    ImagesPage()
 }
